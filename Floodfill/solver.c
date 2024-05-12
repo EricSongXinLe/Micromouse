753fd3f2f2ed7ff0d9bf21f75ec9b3147af5a5b6
EricS <song_xinle@163.com>
2024-05-11 17:53:53 -0700

    floodfill setup code

--- Floodfill/solver.c
+++ Floodfill/solver.c
@@ -1,5 +1,8 @@
 #include "solver.h"
 #include "API.h"
+#include <math.h>
+#include "queue.h"
+#include <stdio.h>
 
 Action solver() {
     return floodFill();
@@ -19,5 +22,17 @@ Action leftWallFollower() {
 
 // Put your implementation of floodfill here!
 Action floodFill() {
+    struct Queue* queue = createQueue();
+    int manhattanDist[16][16];
+    int horizWalls[16][16];
+    int vertWalls[16][16];
+    for(int i = 0; i < 16; i++) {
+        for(int j = 0; j < 16; j++) {
+            manhattanDist[i][j] = abs(i - 7) + abs(j - 7);
+            printf("%d\n", manhattanDist[i][j]);
+            horizWalls[i][j] = 0;
+            vertWalls[i][j] = 0;
+        }
+    }
     return IDLE;
 }
